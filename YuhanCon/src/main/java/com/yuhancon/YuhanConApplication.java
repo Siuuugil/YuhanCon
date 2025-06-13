b9e8caf4f33fd463ce6648076f2640f055a8b32e
youjiyeon12 <youjiyeon12@gmail.com>
2025-06-13 23:35:51 +0900

    프로젝트 생성

---
+++ YuhanCon/src/main/java/com/yuhancon/YuhanConApplication.java
@@ -0,0 +1,13 @@
+package com.yuhancon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuhanConApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuhanConApplication.class, args);
+	}
+
+}
---
+++ YuhanCon/src/test/java/com/yuhancon/YuhanConApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuhancon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuhanConApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
