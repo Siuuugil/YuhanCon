2daf3798bd7453e3ff33950c1954c082180b3ac9
Sookil <net_cityrat@naver.com>
2025-06-14 23:46:40 +0900

    로그인 및 스프링 시큐리티 추가

--- YuhanCon/src/main/java/com/yuhancon/controller/MainController.java
+++ YuhanCon/src/main/java/com/yuhancon/controller/MainController.java
@@ -4,11 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class TestController {
+public class MainController {
+	
 	@GetMapping("/main")
-	public String Mappingtest() {
-		
-		return "main";
-	}
-
+    public String mainPage() {
+        return "main";
+    }
 }
